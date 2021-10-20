6cbcde5e09c2b88a2ef03057f417b430b2ef3c98
seralekseenko <seralekseenko@ukr.net>
2021-10-20 14:13:14 +0300

    initialize classes_example

---
+++ src/main/java/examples/classes_instances/House.java
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+package examples.classes_instances;
+
+class House {
+  int height;
+  int length;
+  int width;
+  String tag;
+}
---
+++ src/main/java/examples/classes_instances/HouseUsage.java
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+package examples.classes_instances;
+
+class HouseUsage {
+
+  public static void main(String[] args) {
+    House sampleHouse = new House();
+
+  }
+
+}
